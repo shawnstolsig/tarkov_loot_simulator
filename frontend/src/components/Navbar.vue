eecbf3ee200e18ab116a9f436402a82d8272a47d
Shawn Stolsig <shawnstolsig@gmail.com>
2020-03-03 23:37:16 -0800

    working data pull from tarkov market

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+    <nav>
+        <v-app-bar app>
+            
+            <!-- Site logo/title -->
+            <v-toolbar-title>
+                <span class="title">EFT: Loot Trainer</span>
+            </v-toolbar-title>
+
+            <v-spacer></v-spacer>
+        
+            <!-- Toolbar buttons -->
+            <v-toolbar-items>
+                <v-btn text to="/" class="mr-2">
+                    <span>Home</span>
+                    <v-icon right>mdi-home</v-icon>
+                </v-btn>
+                <v-btn text to="/about" class="mr-2">
+                    <span>About</span>
+                    <v-icon right >mdi-information</v-icon>
+                </v-btn>
+            </v-toolbar-items>
+        </v-app-bar>
+    </nav>
+</template>
+
+
+<script>
+
+
+export default {
+    data(){
+        return {
+         
+        }
+    },  // end data
+    methods: {
+        
+    },  // end methods
+    computed: {
+       
+    }   // end computed
+}
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,81 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<v-container>
+		<v-card>
+			<v-card-title>
+				Home page (testing)
+			</v-card-title>
+			<v-card-text>
+				<v-btn @click="showItem">
+					Test Tarkov Market API
+				</v-btn>
+			</v-card-text>
+			<v-card-actions>
+				<v-text-field
+					label="Input"
+					v-model="inputString"
+				></v-text-field>
+				<v-text-field
+					v-model="shortName"
+					outlined
+					readonly
+					label="Short Name"
+				></v-text-field>
+				<v-text-field
+					v-model="price"
+					outlined
+					readonly
+					label="24hr Avg Price"
+				></v-text-field>
+				<v-img :src="image"></v-img>
+			</v-card-actions>
+		</v-card>
+	</v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+	name: 'Home',
+	components: {
+		
+	},
+	data(){
+		return {
+			inputString: null,
+			shortName: '(blank)',
+			price: '(blank)',
+			image: '',
+		}
+	},  // end data
+	methods: {
+
+		// get item info from Tarkov Market API
+		showItem(){
+			console.log(`Pulling Tarkov Market for ${this.inputString}`)
+			axios({
+				method: 'get',
+				url: `https://tarkov-market.com/api/v1/item`,
+				params: {
+					q: this.inputString,
+				},
+				headers: {'x-api-key': 'm07rRZrjmjRMHtsP'},
+				
+			})
+			.then(response => {
+				console.log(response)
+				if(response.data.length > 0 ){
+					this.shortName = response.data[0].shortName
+					this.price = response.data[0].avg24hPrice
+					this.image = response.data[0].icon
+				}
+			})
+			.catch(error => {console.log(error)})
+		}
+	},  // end methods
+	computed: {
+	
+	}   // end computed   
+
 }
 </script>
